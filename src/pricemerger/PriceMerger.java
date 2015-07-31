8111df398d7af38567d9fa917e12df6198c8502d
Vanya Kiskin <snarovivan@gmail.com>
2015-07-31 20:06:11 +0300

    Начало положено

---
+++ src/pricemerger/PriceMerger.java
@@ -0,0 +1,40 @@
+/*
+ * ООО "ТК ЭЛЬДОРАДО"
+ * Витебск 2015 * 
+ * Автор: Снаров И.А.
+ */
+
+package pricemerger;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ * Класс представляет само приложение. Структура данных класса соответсвует структуре приложения.
+ * 
+ * @author snarov
+ */
+public class PriceMerger extends Application {
+	
+	
+	private final static String DEF_LANG = "ru";
+	private final static String DEF_LOCATION = "RU";
+	private static final String LOCALE_PATH = "pricemerger/bundles/locale";
+	
+	private ResourceBundle resources = ResourceBundle.getBundle(LOCALE_PATH, new Locale(DEF_LANG, DEF_LOCATION));
+	
+	@Override
+	public void start(Stage stage) throws Exception {
+		Parent root = FXMLLoader.load(getClass().getResource("view/PriceMerger.fxml"), resources);
+		
+		Scene scene = new Scene(root);
+		
+		stage.setScene(scene);
+		stage.show();
+	}
+}
---
+++ src/pricemerger/controller/PriceMergerController.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pricemerger.controller;
+
+import javafx.fxml.FXML;
+
+/**
+ *
+ * @author snarov
+ */
+public class PriceMergerController {
+
+	@FXML
+	private void handleFileOpenMaster() {
+	}
+
+	@FXML
+	private void handleFileOpenMerge() {
+	}
+
+	@FXML
+	private void handleFileExit() {
+	}
+}
